8e706076ac0ebfacfe391bd2b75630eab9d37a68
leen <programmerleen@gmail.com>
2025-07-22 21:39:45 -0700

    project

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,19 +1,62 @@
 import React, { useState } from "react";
 import { Text, View, StyleSheet } from "react-native";
 import SearchBar from "../Components/SearchBar";
+import yelp from "../api/yelp";
+
 const SearchScreen = () => {
-  // prettier-ignore
-  const [term , setTerm] = useState('');
+  const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const searchApi = async () => {
+    try {
+      const response = await yelp.get("/search", {
+        params: {
+          limit: 50,
+          term: term,
+          location: "san jose",
+        },
+      });
+
+      if (response.data.businesses.length === 0) {
+        setErrorMessage(" Something went wrong");
+      } else {
+        setErrorMessage("");
+      }
+
+      setResults(response.data.businesses);
+    } catch (err) {
+      setErrorMessage("Something  Went Wrong ");
+    }
+  };
+
   return (
     <View>
-      <SearchBar
-        term={term}
-        onTermChange={setTerm}
-        onTermSubmit={() => console.log("term was submitted")}
-      />
-      <Text style={{ fontSize: 25, color: "#fd5722" }}>Search Screen</Text>
-      <Text>{term}</Text>
+      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
+
+      {errorMessage ? (
+        <Text style={styles.errorText}>{errorMessage}</Text>
+      ) : null}
+
+      <Text style={styles.resultText}>
+        We have found {results.length} results
+      </Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  errorText: {
+    fontSize: 18,
+    color: "#fd5722",
+    marginHorizontal: 10,
+    marginTop: 10,
+  },
+  resultText: {
+    marginHorizontal: 10,
+    marginTop: 10,
+    fontSize: 16,
+  },
+});
+
 export default SearchScreen;
